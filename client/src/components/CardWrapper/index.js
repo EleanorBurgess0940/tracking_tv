f6bfb83398214737f505c3a17e43593001c724d0
Reformed Scribe <stmiller57@gmail.com>
2020-07-20 16:24:23 -0400

    Added a MovieCard component andCardDeck wrapper, included links to Bootstrap

---
+++ client/src/components/CardWrapper/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Wrapper(props) {
+    return <div>
+        <div className="wrapper"></div>
+        <div className="row"></div>
+        <div className="col-sm"></div>
+        <div className="card-deck">{props.children}</div>;
+        </div>
+}
+
+export default Wrapper;
