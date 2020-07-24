775079cb6fd708860884379fa6d00737002551bc
Eleanor Burgess <eleanor.c.burgess@gmail.com>
2020-07-23 20:05:05 -0400

    added functionality to the login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import axios from "axios";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
@@ -9,35 +10,77 @@ import CardWrapper from "./components/CardWrapper";
 import signUp from "./components/signup/signup.js";
 import Login from "./components/login/login.js";
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Nav />
-        <Switch>
-          <Route path="/signup" component={signUp} />
-          <Route path="/login" component={Login} />
-        </Switch>
-        <CardWrapper>
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-        </CardWrapper>
-        <CardWrapper>
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-        </CardWrapper>
-        <Footer />
-      </div>
-    </Router>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loggedIn: false,
+      username: null,
+    };
+
+    this.getUser = this.getUser.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.updateUser = this.updateUser.bind(this);
+  }
+
+  componentDidMount() {
+    this.getUser();
+  }
+
+  updateUser(userObject) {
+    this.setState(userObject);
+  }
+
+  getUser() {
+    axios.get("/user/").then((response) => {
+      console.log("Get user response: ");
+      console.log(response.data);
+      if (response.data.user) {
+        console.log("Get User: There is a user saved in the server session: ");
+
+        this.setState({
+          loggedIn: true,
+          username: response.data.user.username,
+        });
+      } else {
+        console.log("Get user: no user");
+        this.setState({
+          loggedIn: false,
+          username: null,
+        });
+      }
+    });
+  }
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Nav />
+          <Switch>
+            <Route path="/signup" component={signUp} />
+            <Route path="/login" component={Login} />
+          </Switch>
+          <CardWrapper>
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+          </CardWrapper>
+          <CardWrapper>
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+          </CardWrapper>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
