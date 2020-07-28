18b5a89ae885556c524bdaa7b1f00c4167240abf
Eleanor Burgess <eleanor.c.burgess@gmail.com>
2020-07-27 20:13:29 -0400

    added a no image found if there is no poster prop

--- client/src/components/TvCard/index.js
+++ client/src/components/TvCard/index.js
@@ -1,21 +1,38 @@
 import React from "react";
 import "./style.css";
+import noImage from "../../images/noImageFound.png";
 
 function TvCard(props) {
-  console.log("hello");
-  return (
-    <div className="card text-white bg-dark">
-      <div className="card-image">
-        <img
-          alt={props.name}
-          src={"https://image.tmdb.org/t/p/w500/" + props.poster}
-          className="img-responsive card-img-top"
-          width="100%"
-        />
-        <h1>{props.name}</h1>
+  console.log(props);
+  if (props.poster) {
+    return (
+      <div className="card text-white bg-dark">
+        <div className="card-image">
+          <img
+            alt={props.name}
+            src={"https://image.tmdb.org/t/p/w500/" + props.poster}
+            className="img-responsive card-img-top"
+            width="100%"
+          />
+          <h1>{props.name}</h1>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="card text-white bg-dark">
+        <div className="card-image">
+          <img
+            alt={props.name}
+            src={noImage}
+            className="img-responsive card-img-top"
+            width="100%"
+          />
+          <h1>{props.name}</h1>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default TvCard;
