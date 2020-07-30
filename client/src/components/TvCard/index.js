888391a56a1660b5b6ef7f01d15a17bc11ea5f64
Reformed Scribe <stmiller57@gmail.com>
2020-07-30 18:51:31 -0400

    Fixed margins between cards

--- client/src/components/TvCard/index.js
+++ client/src/components/TvCard/index.js
@@ -6,43 +6,48 @@ function TvCard(props) {
   console.log(props);
   if (props.poster) {
     return (
-      <div className="col card text-white">
-        <div className="card-image">
-          <img
-            alt={props.name}
-            src={"https://image.tmdb.org/t/p/w500/" + props.poster}
-            className="img-responsive card-img-top"
-            width="100%"
-          />
-        </div>
-        <div className="card-body text-white">
-          <h4 id="show-title">{props.name}</h4>
-        </div>
-        <div>
-          <button type="button" className="btn btn-outline-light">
-            Average rating: {props.rating}
-          </button>
+      <div className="col-sm-3">
+        <div className="card text-white">
+          <div className="card-image">
+            <img
+              alt={props.name}
+              src={"https://image.tmdb.org/t/p/w500/" + props.poster}
+              className="img-responsive card-img-top"
+              width="100%"
+            />
+          </div>
+          <div className="card-body text-white">
+            <h4 id="show-title">{props.name}</h4>
+          </div>
+          <div>
+            <button type="button" className="btn btn-outline-light">
+              Average rating: {props.rating}
+            </button>
+          </div>
         </div>
       </div>
+
     );
   } else {
     return (
-      <div className="col card text-white">
-        <div className="card-image">
-          <img
-            alt={props.name}
-            src={noImage}
-            className="img-responsive card-img-top"
-            width="100%"
-          />
-        </div>
-        <div className="card-body text-white">
-          <h4 id="show-title">{props.name}</h4>
-        </div>
-        <div>
-          <button type="button" className="btn btn-outline-light">
-            Avg. rating: {props.rating}
-          </button>
+      <div className="col-sm-3">
+        <div className="card text-white">
+          <div className="card-image">
+            <img
+              alt={props.name}
+              src={noImage}
+              className="img-responsive card-img-top"
+              width="100%"
+            />
+          </div>
+          <div className="card-body text-white">
+            <h4 id="show-title">{props.name}</h4>
+          </div>
+          <div>
+            <button type="button" className="btn btn-outline-light">
+              Avg. rating: {props.rating}
+            </button>
+          </div>
         </div>
       </div>
     );
