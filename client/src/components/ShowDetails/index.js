a78c0e426995bc7a2092f6d24df1e160e72005cc
Reformed Scribe <stmiller57@gmail.com>
2020-08-05 13:12:46 -0400

    Adjusted style of page with more show info

--- client/src/components/ShowDetails/index.js
+++ client/src/components/ShowDetails/index.js
@@ -7,21 +7,27 @@ function showdetails(props) {
     <div>
       <div className="wrapper">
         <div className="row">
+          <div className="col-sm-3">
+          </div>
           <div className="col-sm-6">
             <div className="card">
               <h2 className="card-header">{props.state.tvshow}</h2>
               <img className="card-img-top"
                 alt={props.name}
                 src={"https://image.tmdb.org/t/p/w500/" + props.state.tvposter} />
+              <h3 className="card-header">Overview</h3>
+              <br></br>
+              <h6>{props.state.overview}</h6>
               <h3>
                 <span className="badge badge-pill">Average viewer rating: {props.state.voteAverage}</span>
               </h3>
-            </div>
-          </div>
-          <div className="col-sm-6">
-            <div className="card">
-              <h3>Overview</h3>
-              <h5>{props.state.overview}</h5>
+              <br></br>
+              <h4 className="card-header">More details</h4>
+              <br></br>
+              <p>No. of seasons: {props.state.numberOfSeasons} </p>
+              <p>No. of episodes: {props.state.numberOfEpisodes} </p>
+              <p>Homepage: {props.state.homepage} </p>
+              <p>Status: {props.state.status}  </p>
             </div>
           </div>
         </div>
@@ -47,3 +53,16 @@ export default showdetails;
                 </h3>
               </div>
             </div> */}
+
+
+{/* <div className="col-sm-6">
+            <div className="card">
+              <h2 className="card-header">{props.state.tvshow}</h2>
+              <img className="card-img-top"
+                alt={props.name}
+                src={"https://image.tmdb.org/t/p/w500/" + props.state.tvposter} />
+              <h3>
+                <span className="badge badge-pill">Average viewer rating: {props.state.voteAverage}</span>
+              </h3>
+            </div>
+          </div> */}
